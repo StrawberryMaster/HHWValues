3b2d43f675993ca62c5ad7466d70a0e359924eb8
Felipe <41008398+StrawberryMaster@users.noreply.github.com>
2023-09-15 16:54:19 +0000

    Cleanup

--- scripts/index.js
+++ scripts/index.js
@@ -1,21 +1,35 @@
+const CANVAS_WIDTH = 1850;
+const CANVAS_HEIGHT = 1300;
+const BACKGROUND_COLOR = "#eee"
+const BACKGROUND_IMAGE_PATH = "./assets/compass.png";
+const BANNER_ELEMENT_ID = "banner";
+
 const numOfQuestionsElement = document.getElementById("numOfQuestions");
 const numOfQuestions = questionsOrder.length;
 numOfQuestionsElement.textContent = numOfQuestions;
 
-function drawCanvas() {
-  const c = document.createElement("canvas");
-  c.width = 1850;
-  c.height = 1300;
-  const ctx = c.getContext("2d");
-  ctx.fillStyle = "#EEEEEE";
-  ctx.fillRect(0, 0, 1850, 1300);
+const createCanvas = () => {
+    const c = document.createElement("canvas");
+    c.width = CANVAS_WIDTH;
+    c.height = CANVAS_HEIGHT;
+    const ctx = c.getContext("2d");
+    ctx.fillStyle = BACKGROUND_COLOR;
+    ctx.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+    return { c, ctx };
+};
 
-  const background = new Image();
-  background.onload = function () {
-    ctx.drawImage(background, 0, 0);
-    document.getElementById("banner").src = c.toDataURL();
-  };
-  background.src = "./assets/compass.png";
-}
+const loadAndDrawBackground = (ctx, c) => {
+    const background = new Image();
+    background.onload = () => {
+        ctx.drawImage(background, 0, 0);
+        document.getElementById(BANNER_ELEMENT_ID).src = c.toDataURL();
+    };
+    background.src = BACKGROUND_IMAGE_PATH;
+};
+
+const drawCanvas = () => {
+    const { c, ctx } = createCanvas();
+    loadAndDrawBackground(ctx, c);
+};
 
 document.addEventListener("DOMContentLoaded", drawCanvas);
--- scripts/results.js
+++ scripts/results.js
@@ -2,30 +2,37 @@ const urlParams = new URLSearchParams(window.location.search);
 const xAxis = parseFloat(urlParams.get("right"));
 const yAxis = parseFloat(urlParams.get("strict"));
 
+const CANVAS_WIDTH = 1850;
+const CANVAS_HEIGHT = 1600;
+const FONT_FAMILY = "Georgia, serif";
+
 const canvasElement = document.createElement("canvas");
-canvasElement.width = 1850;
-canvasElement.height = 1600;
+canvasElement.width = CANVAS_WIDTH;
+canvasElement.height = CANVAS_HEIGHT;
 
 const imageElement = new Image();
 imageElement.src = "./assets/compass.png";
 
 const bannerElement = document.getElementById("banner");
 
-function drawCanvas() {
+const drawText = (ctx, text, x, y, align = "left", color = "#fff", fontSize = "5.75em") => {
+    ctx.fillStyle = color;
+    ctx.textAlign = align;
+    ctx.font = `${fontSize} ${FONT_FAMILY}`;
+    ctx.fillText(text, x, y);
+};
+
+const drawCanvas = () => {
     const ctx = canvasElement.getContext("2d");
     ctx.fillStyle = "#EEEEEE";
-    ctx.fillRect(0, 0, 1850, 1600);
+    ctx.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
     ctx.drawImage(imageElement, 0, 100);
     
     ctx.fillStyle = "#c03";
-    ctx.fillRect(0, 0, 1850, 100);
+    ctx.fillRect(0, 0, CANVAS_WIDTH, 100);
 
-    ctx.fillStyle = "#fff";
-    ctx.textAlign = "left";
-    ctx.font = "5.75em Georgia, serif";
-    ctx.fillText("HHWValues", 30, 70);
-    ctx.textAlign = "right";
-    ctx.fillText("strawberrymaster.github.io/HHWValues", 1820, 70);
+    drawText(ctx, "HHWValues", 30, 70);
+    drawText(ctx, "strawberrymaster.github.io/HHWValues", 1820, 70, "right");
 
     ctx.beginPath();
     ctx.arc(425 + (1000 * (xAxis + 10) / 20), 1250 - (1000 * (yAxis + 10) / 20), 25, 0, 2 * Math.PI);
@@ -33,13 +40,10 @@ function drawCanvas() {
     ctx.fill();
     ctx.stroke();
 
-    ctx.font = "6em Georgia, serif";
-    ctx.textAlign = "center";
-    ctx.fillStyle = "#222222";
-    ctx.fillText("Progressive / Traditional Axis (x): " + xAxis, 925, 1475);
-    ctx.fillText("Lenient / Strict Axis (y): " + yAxis, 925, 1550);
+    drawText(ctx, `Progressive / Traditional Axis (x): ${xAxis}`, 925, 1465, "center", "#222222", "6em");
+    drawText(ctx, `Lenient / Strict Axis (y): ${yAxis}`, 925, 1540, "center", "#222222", "6em");
 
     bannerElement.src = canvasElement.toDataURL();
-}
+};
 
 imageElement.onload = () => window.requestAnimationFrame(drawCanvas);
