b3fe993ec12a7a588816dea3a0ac7d74e4b020b8
Felipe <41008398+StrawberryMaster@users.noreply.github.com>
2024-07-13 21:01:38 +0000

    Tinkering with this

--- scripts/questions.js
+++ scripts/questions.js
@@ -2,303 +2,303 @@ questions = [{
   "id": 0,
   "question": "HHW needs Storm Mode to prevent jokes and arguments during a threatening tropical cyclone.",
   "effects": {
-    "strict": 1
+    "strict": 2
   }
 }, {
   "id": 1,
   "question": "HHW should strongly encourage, or even require, users to support \"basin neutrality\".",
   "effects": {
-    "right": -1,
-    "strict": 0.5
+    "right": -2,
+    "strict": 1
   }
 }, {
   "id": 2,
   "question": "Staff members that do not moderate the chat frequently enough should be demoted.",
   "effects": {
-    "right": -1
+    "right": -2
   }
 }, {
   "id": 3,
   "question": "COPPA, which disallows users under 13 from joining the wiki, should not be followed.",
   "effects": {
-    "right": -1
+    "right": -2
   }
 }, {
   "id": 4,
   "question": "Measures like staff reductions and other role changes are beneficial to the community.",
   "effects": {
-    "right": -1,
-    "strict": 0.5
+    "right": -2,
+    "strict": 1
   }
 }, {
   "id": 5,
   "question": "The wiki leader/server owner should be able to make decisions by themself without a consensus.",
   "effects": {
-    "strict": 1
+    "strict": 2
   }
 }, {
   "id": 6,
   "question": "Complaining and/or ranting about tropical cyclones or weather is childish behavior and should be punished.",
   "effects": {
-    "right": -1
+    "right": -2
   }
 }, {
   "id": 7,
   "question": "The category difference between a 155 mph and 160 mph hurricane is important.",
   "effects": {
-    "right": 1
+    "right": 2
   }
 }, {
   "id": 8,
   "question": "Users should always focus their attention on the more threatening storm to track, regardless of what basin it is in.",
   "effects": {
-    "right": -1
+    "right": -2
   }
 }, {
   "id": 9,
   "question": "HHW needs to move to an external site (in the mold of Hypoverse) or become independent from Fandom.",
   "effects": {
-    "right": -1
+    "right": -2
   }
 }, {
   "id": 10,
   "question": "I support a 1-1-1-1-1 administration team.",
   "effects": {
-    "right": -1
+    "right": -2
   }
 }, {
   "id": 11,
   "question": "Users should be given indefinite bans when it is not clear if they will improve.",
   "effects": {
-    "strict": 1
+    "strict": 2
   }
 }, {
   "id": 12,
   "question": "NSFW discussion should not be punished with mutes right away.",
   "effects": {
-    "strict": -1
+    "strict": -2
   }
 }, {
   "id": 13,
   "question": "Memes should be allowed in #casual-chat.",
   "effects": {
-    "strict": -0.5
+    "strict": -1
   }
 }, {
   "id": 14,
   "question": "HHW chat was more fun several years ago than it is today.",
   "effects": {
-    "right": 1
+    "right": 2
   }
 }, {
   "id": 15,
   "question": "The fanfic era was good and should be revived.",
   "effects": {
-    "right": -1
+    "right": -2
   }
 }, {
   "id": 16,
   "question": "Random foreign language words should not be allowed in #casual-chat.",
   "effects": {
-    "strict": 1
+    "strict": 2
   }
 }, {
   "id": 17,
   "question": "Former moderators who broke Discord's Terms of Service should be allowed a chance of being promoted to staff later on.",
   "effects": {
-    "strict": -1
+    "strict": -2
   }
 }, {
   "id": 18,
   "question": "The wiki is more important than the server.",
   "effects": {
-    "right": -0.5
+    "right": -1
   }
 }, {
   "id": 19,
   "question": "Measures to control disruptive behavior are usually bad.",
   "effects": {
-    "strict": -0.5
+    "strict": -1
   }
 }, {
   "id": 20,
   "question": "People I dislike should be banned, even if they haven't violated any rules.",
   "effects": {
-    "right": -0.5,
-    "strict": 1
+    "right": -1,
+    "strict": 2
   }
 }, {
   "id": 21,
   "question": "Outsiders who are not active on the wiki should not have any influence on major wiki decisions.",
   "effects": {
-    "right": -1
+    "right": -2
   }
 }, {
   "id": 22,
   "question": "All staff should participate in yearly or bi-yearly elections.",
   "effects": {
-    "right": -1
+    "right": -2
   }
 }, {
   "id": 23,
   "question": "Administration members should be able to jump positions in an unorthodox way.",
   "effects": {
-    "right": -1
+    "right": -2
   }
 }, {
   "id": 24,
-  "question": "Trials are good and should be used more often.",
+  "question": "Trials are good and should be used more often, including on all future staff promotions.",
   "effects": {
-    "right": -0.5
+    "right": -1
   }
 }, {
   "id": 25,
   "question": "GIFs should be allowed in #casual-chat.",
   "effects": {
-    "strict": -0.5
+    "strict": -1
   }
 }, {
   "id": 26,
   "question": "Hurricane Idol rounds should be decided by HHW users who are eligible to vote, and not the judges.",
   "effects": {
-    "right": -1
+    "right": -2
   }
 }, {
   "id": 27,
   "question": "Suicide and LGBTQ+ related jokes should be allowed in the server.",
   "effects": {
-    "right": -1
+    "right": -2
   }
 }, {
   "id": 28,
   "question": "The HHW Electoral College (HHW EC) should be abolished.",
   "effects": {
-    "right": -0.5
+    "right": -1
   }
 }, {
   "id": 29,
   "question": "We should create stricter policies for Discussions.",
   "effects": {
-    "right": -0.5,
-    "strict": 0.5
+    "right": -1,
+    "strict": 1
   }
 }, {
   "id": 30,
   "question": "Community consensus should help guide future staff decisions.",
   "effects": {
-    "right": -0.5,
-    "strict": -0.5
+    "right": -1,
+    "strict": -1
   }
 }, {
   "id": 31,
   "question": "The current amournt of staff surveys is excessive and should be scaled back again to two surveys per year.",
   "effects": {
-    "right": 0.5
+    "right": 1
   }
 }, {
   "id": 32,
   "question": "Long-time problematic users should have the ability to appeal their ban, even if they are likely to get banned after returning.",
   "effects": {
-    "strict": -0.5
+    "strict": -1
   }
 }, {
   "id": 33,
   "question": "Current staff measures to control disruption in the chat do not go far enough.",
   "effects": {
-    "strict": -0.5,
-    "right": -0.25
+    "strict": -1,
+    "right": -0.5
   }
 }, {
   "id": 34,
   "question": "Rules regarding what can be publicly sent from #staff and #administration should be less strict.",
   "effects": {
-    "strict": -0.5
+    "strict": -1
   }
 }, {
   "id": 35,
   "question": "The moderator conduct policies should be more frequently enforced.",
   "effects": {
-    "strict": 0.5,
-    "right": -0.25
+    "strict": 1,
+    "right": -0.5
   }
 }, {
   "id": 35,
   "question": "Tickets and reports are an effective method of detecting incidents of rule-breaking behavior.",
   "effects": {
-    "right": -0.25
+    "right": -0.5
   }
 }, {
   "id": 36,
   "question": "Certain group chats should not have any influence over staff decisions.",
   "effects": {
-    "strict": -0.25,
-    "right": 1
+    "strict": -0.5,
+    "right": 2
   }
 }, {
   "id": 37,
   "question": "The current staff team is unfairly biased against certain users because of their beliefs.",
   "effects": {
-    "right": 0.5
+    "right": 1
   }
 }, {
   "id": 38,
   "question": "Politics-related conversations do not belong in the wiki and/or server.",
   "effects": {
-    "right": 0.25
+    "right": 0.5
   }
 }, {
   "id": 39,
   "question": "The HHW Discord server needs more channels, even if there isn't a demand for it.",
   "effects": {
-    "right": -0.5,
-    "strict": -0.5
+    "right": -1,
+    "strict": -1
   }
 }, {
   "id": 40,
   "question": "Appeals should be handled by an independent HHW Arbitration Committee, and not the administration.",
   "effects": {
-    "right": 0.5,
-    "strict": 0.25
+    "right": 1,
+    "strict": 0.5
   }
 }, {
   "id": 41,
   "question": "The administration should ask Fandom Staff to remove Discussions from the wiki.",
   "effects": {
-    "right": -1,
-    "strict": 0.25
+    "right": -2,
+    "strict": 0.5
   }
 }, {
   "id": 41,
   "question": "Former administration members should once again have an input in administration decisions.",
   "effects": {
-    "right": 1,
-    "strict": -0.25
+    "right": 2,
+    "strict": -0.5
   }
 }, {
   "id": 42,
   "question": "The strike system should be repealed, and punishments should be determined on a case-by-case basis.",
   "effects": {
-    "right": 0.5,
-    "strict": -0.25
+    "right": 1,
+    "strict": -0.5
   }
 }, {
   "id": 43,
   "question": "HHW needs a community advisor to foster closer connections with the community.",
   "effects": {
-    "right": -0.25,
+    "right": -0.5,
   }
 }, {
   "id": 44,
   "question": "User rights requirements are overly strict and should be relaxed.",
   "effects": {
-    "strict": -0.25,
-    "right": -0.5
+    "strict": -0.5,
+    "right": -1
   }
 }, {
   "id": 45,
   "question": "Users should be aware of each staff member's positions on administration votes.",
   "effects": {
-    "strict": -0.25,
-    "right": -1
+    "strict": -0.5,
+    "right": -2
   }
 }];
